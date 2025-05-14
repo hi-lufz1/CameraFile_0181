ef6fda8c91b642a3b6302aeb57923f761290e004
hi-lufz1 <latifufauzi@gmail.com>
2025-05-15 01:24:35 +0700

    membuat file FullPage

---
+++ lib/full_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FullPage extends StatefulWidget {
+  const FullPage({super.key});
+
+  @override
+  State<FullPage> createState() => _FullPageState();
+}
+
+class _FullPageState extends State<FullPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
