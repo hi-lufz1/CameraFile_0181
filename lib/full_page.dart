7a37864888e596dd268e41199417d72f639036fe
hi-lufz1 <latifufauzi@gmail.com>
2025-05-15 08:33:34 +0700

    menambahkan fungsi mengambil gambar dari galeri

--- lib/full_page.dart
+++ lib/full_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:camera_file/camera_page.dart';
 import 'package:camera_file/storage_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class FullPage extends StatefulWidget {
@@ -33,6 +34,18 @@ class _FullPageState extends State<FullPage> {
       ScaffoldMessenger.of(
         context,
       ).showSnackBar(SnackBar(content: Text('Disimpan: ${saved.path}')));
+    }
+  }
+
+  Future<void> _pickFromGallery() async {
+    final picker = ImagePicker();
+    final picked = await picker.pickImage(source: ImageSource.gallery);
+    if (picked != null) {
+      final saved = await StorageHelper.saveImage(File(picked.path), 'gallery');
+      setState(() => _imageFile = saved);
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Disalin: ${saved.path}')));
     }
   }
 
