0629d09d292389ab6ccf2a4939388d269f3618f2
hi-lufz1 <latifufauzi@gmail.com>
2025-05-15 21:53:19 +0700

    menambahkan fungsi untuk fokus dan memilih gambar dari galeri

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -4,6 +4,8 @@ import 'package:bloc/bloc.dart';
 import 'package:camera/camera.dart';
 import 'package:camera_file/bloc/camera_event.dart';
 import 'package:camera_file/bloc/camera_state.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:meta/meta.dart';
 
 part 'camera_event.dart';
@@ -65,5 +67,32 @@ class CameraBloc extends Bloc<CameraEvent, CameraState> {
     final s = state as CameraReady;
     final file = await s.controller.takePicture();
     event.onPictureTaken(File(file.path));
+  }
+
+  Future<void> _onTapFocus(TapToFocus event, Emitter<CameraState> emit) async {
+    if (state is! CameraReady) return;
+    final s = state as CameraReady;
+    final relative = Offset(
+      event.position.dx / event.previewSize.width,
+      event.position.dy / event.previewSize.height,
+    );
+    await s.controller.setFocusPoint(relative);
+    await s.controller.setExposurePoint(relative);
+  }
+
+  Future<void> _onPickGallery(
+    PickImageFromGallery event,
+    Emitter<CameraState> emit,
+  ) async {
+    if (state is! CameraReady) return;
+    final picker = ImagePicker();
+    final picked = await picker.pickImage(source: ImageSource.gallery);
+    final file = File(picked!.path);
+    emit(
+      (state as CameraReady).copyWith(
+        imageFile: file,
+        snackBarMessage: 'Berhasil memilih dari galeri',
+      ),
+    );
   }
 }
