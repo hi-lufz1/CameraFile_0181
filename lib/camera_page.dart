3822e4ed4bae6754be587d30e276c6ae5ef30a5f
hi-lufz1 <latifufauzi@gmail.com>
2025-05-15 22:45:33 +0700

    menambahkan inisialisasi kamera

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,3 +1,6 @@
+import 'package:camera_file/bloc/camera_bloc.dart';
+import 'package:camera_file/bloc/camera_event.dart';
+import 'package:camera_file/bloc/camera_state.dart';
 import 'package:flutter/material.dart';
 
 class CameraPage extends StatefulWidget {
@@ -9,6 +12,15 @@ class CameraPage extends StatefulWidget {
 
 class _CameraPageState extends State<CameraPage> {
   @override
+    void initState() {
+    super.initState();
+    final bloc = context.read<CameraBloc>();
+    if (bloc.state is! CameraReady) {
+      bloc.add(InitializeCamera());
+    }
+  }
+
+
   Widget build(BuildContext context) {
     return const Placeholder();
   }
