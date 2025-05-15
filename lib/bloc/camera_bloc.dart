ba38ca55034fed0cd37b7268c46bbad1f4a7a4a0
hi-lufz1 <latifufauzi@gmail.com>
2025-05-15 22:16:20 +0700

    menambahkan fungsi untuk membersihkan snackbar dan memperbarui controller kamera

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -148,4 +148,32 @@ class CameraBloc extends Bloc<CameraEvent, CameraState> {
       ),
     );
   }
+
+  Future<void> _onClearSnackbar(
+      ClearSnackbar event, Emitter<CameraState> emit) async {
+    if (state is! CameraReady) return;
+    final s = state as CameraReady;
+    emit(s.copyWith(clearSnackbar: true));
+  }
+
+  Future<void> _setupController(
+    int index,
+    Emitter<CameraState> emit, {
+      CameraReady? previous,
+    }) async {
+      await previous?.controller.dispose();
+      final controller = CameraController(_cameras[index], ResolutionPreset.max,
+          enableAudio: false);
+      await controller.initialize();
+      await controller.setFlashMode(previous?.flashMode ?? FlashMode.off);
+
+      emit(CameraReady(
+        controller: controller, 
+        selectedIndex: index, 
+        flashMode: previous?.flashMode ?? FlashMode.off,
+        imageFile: previous?.imageFile,
+        snackBarMessage: null,
+      )
+    );
+  }
 }
