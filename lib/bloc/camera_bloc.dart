58ce150ffd4f60df798b276049988e80a4590660
hi-lufz1 <latifufauzi@gmail.com>
2025-05-15 21:18:39 +0700

    menambahkan event handler

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -1,13 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:camera/camera.dart';
+import 'package:camera_file/bloc/camera_event.dart';
+import 'package:camera_file/bloc/camera_state.dart';
 import 'package:meta/meta.dart';
 
 part 'camera_event.dart';
 part 'camera_state.dart';
 
 class CameraBloc extends Bloc<CameraEvent, CameraState> {
+  late final List<CameraDescription> _cameras;
+
   CameraBloc() : super(CameraInitial()) {
-    on<CameraEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<InitializeCamera>(_onInit);
+    on<SwitchCamera>(_onSwitch);
+    on<ToogleFlash>(_onToggleFlash);
+    on<TakePicture>(_onTakePicture);
+    on<TapToFocus>(_onTapFocus);
+    on<PickImageFromGallery>(_onPickGallery);
+    on<OpenCameraAndCapture>(_onOpenCamera);
+    on<DeleteImage>(_onDeleteImage);
+    on<ClearSnackbar>(_onClearSnackbar);
+    on<RequestPermission>(_onRequestPermissions);
   }
 }
