0fdb3b6569af9d76b5d4c89b5347075ffeee05c5
hi-lufz1 <latifufauzi@gmail.com>
2025-05-15 07:52:20 +0700

    menambahkan permintaan izin untuk akses kamera dan penyimpanan

--- lib/full_page.dart
+++ lib/full_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class FullPage extends StatefulWidget {
   const FullPage({super.key});
@@ -8,6 +11,14 @@ class FullPage extends StatefulWidget {
 }
 
 class _FullPageState extends State<FullPage> {
+  File? _imageFile;
+
+  Future<void> _requestPermissions() async {
+    await Permission.camera.request();
+    await Permission.storage.request();
+    await Permission.manageExternalStorage.request();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
